c53ba08011d410663c70a0cb25fd589a04893e3c
Mateusz Bukowski <mateuszbukowski13@gmail.com>
2019-10-14 15:05:27 +0200

    Rozpoczęcie kursu JAVA i utworzenie repozytorium GIT.

---
+++ src/Rozdzial_2.java
@@ -0,0 +1,26 @@
+
+public class Rozdzial_2 {
+
+	public static void main(String[] args) {
+		// 2.2 Metody print i println
+		//print i println
+//		System.out.println("Pierwszy tekst!");
+//		System.out.println("Drugi tekst!");
+//		System.out.print("Trzeci ");
+//		System.out.println("tekst!");
+//		//Sekwencja ucieczki
+//		//Umieszczenie instrukcji steruj¹cej bezpoœrednio w tekœcie
+//		System.out.print("Oto najlepiej sprzedaj¹ce siê produkty: \n");
+//		System.out.print(" Gry\n kawa\n Aspiryna\n");
+//		System.out.print("Oto najlepiej sprzedaj¹ce siê produkty: \n");
+//		System.out.print(" Gry\t kawa\t Aspiryna");
+		// 2.3 Zmienne i litera³y
+//		int value;
+//		value = 5;
+//		System.out.print("Zmienna value jest równa ");
+//		System.out.println(value + ".");
+		//2.4 Proste typy danych
+		
+	}
+
+}
---
+++ src/Rozdzial_3.java
@@ -0,0 +1,11 @@
+
+public class Rozdzial_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("test");
+
+		
+	}
+
+}
