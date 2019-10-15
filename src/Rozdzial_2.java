f65894e91a4c9059d13b06dd504b701ead78bbba
Mateusz Bukowski <mateuszbukowski13@gmail.com>
2019-10-15 20:47:45 +0200

    [Rozdział 2] - Ukończone 2.4

--- src/Rozdzial_2.java
+++ src/Rozdzial_2.java
@@ -2,25 +2,69 @@
 public class Rozdzial_2 {
 
 	public static void main(String[] args) {
-		// 2.2 Metody print i println
-		//print i println
+// 2.2 Metody print i println
+//print i println
 //		System.out.println("Pierwszy tekst!");
 //		System.out.println("Drugi tekst!");
 //		System.out.print("Trzeci ");
 //		System.out.println("tekst!");
-//		//Sekwencja ucieczki
-//		//Umieszczenie instrukcji steruj¹cej bezpoœrednio w tekœcie
+//Sekwencja ucieczki
+//Umieszczenie instrukcji steruj¹cej bezpoœrednio w tekœcie
 //		System.out.print("Oto najlepiej sprzedaj¹ce siê produkty: \n");
 //		System.out.print(" Gry\n kawa\n Aspiryna\n");
 //		System.out.print("Oto najlepiej sprzedaj¹ce siê produkty: \n");
 //		System.out.print(" Gry\t kawa\t Aspiryna");
-		// 2.3 Zmienne i litera³y
+// 2.3 Zmienne i litera³y
 //		int value;
 //		value = 5;
 //		System.out.print("Zmienna value jest równa ");
 //		System.out.println(value + ".");
-		//2.4 Proste typy danych
-		// 
+//2.4 Proste typy danych
+// Zmienne ca³kowitoliczbowe
+//		int checking;
+//		byte miles;
+//		short minutes;
+//		long days;
+//		checking = -20;
+//		miles = 105;
+//		minutes = 120;
+//		days = 189000;
+//		System.out.println("Odbyliœmy podróz o d³ugoœci " + miles + " mil.");
+//		System.out.println("Zajê³o nam to " + minutes + " minut.");
+//		System.out.println("Stan konta w z³otych wynosi: " + checking);
+//		System.out.println("Oko³o " + days + " dni temu w tym miejscu" +
+//							" sta³ kolummb.");
+// Zmienne zmiennoprzecinkowe
+//		double price, tax, total;
+//		price = 29.75;
+//		tax = 1.76;
+//		total = 31.51;
+//		System.out.println("Cena produktu " + 
+//		"wynosi " + price + ".");
+//		System.out.println("Podatek wynosi " + tax + "."); 
+//		System.out.println("£¹czna kwota wynosi " + total + "."); 
+// Zmienne boolean
+//		boolean bool;
+//		bool = true;
+//		System.out.println(bool);
+//		bool = false;
+//		System.out.println(bool);
+// Zmienne char - znakowe - tylko jeden znak 
+//		char letter;
+//		letter = 'A'; //Apostrof nie cudzys³ow!!!
+//		System.out.println(letter);
+//		letter = 'B';
+//		System.out.println(letter);
+//		letter = 65;
+//		System.out.println(letter);
+//		letter = 66;
+//		System.out.println(letter);
+// Inicjowanie zmiennej
+		int month = 2, days = 28;
+		System.out.println("Miesi¹c nr " + month + " ma " +
+							days + " dni");
+				
+			
 	}
 
 }
