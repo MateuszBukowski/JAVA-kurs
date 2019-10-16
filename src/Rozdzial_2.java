1c900fe848562adf0395b95e32d0251b85dc6ea8
Mateusz Bukowski <mateuszbukowski13@gmail.com>
2019-10-16 11:24:07 +0200

    [Rozdział 2] - Ukończone 2.4 - 2.12.

--- src/Rozdzial_2.java
+++ src/Rozdzial_2.java
@@ -60,11 +60,132 @@ public class Rozdzial_2 {
 //		letter = 66;
 //		System.out.println(letter);
 // Inicjowanie zmiennej
-		int month = 2, days = 28;
-		System.out.println("Miesi¹c nr " + month + " ma " +
-							days + " dni");
+//		int month = 2, days = 28;
+//		System.out.println("Miesi¹c nr " + month + " ma " +
+//							days + " dni");
+//2.4 Operatory arytmetyczne
+//		int amount, total, price, tax, number, temp, sale, discount;
+//		
+//		amount = 4 + 8;
+//		total = price + tax;
+//		number = number + 1;
+//		
+//		temp = 112 - 14;
+//		sale = sale - discount;
+//		number = number - 1;
+// Program oblicza wyp³atê + dodatek za nadgodziny
+//		double regularWages;
+//		double basePay = 25;
+//		double regularHours = 40;
+//		double overtimeWages;
+//		double overtimePay = 37.5;
+//		double overtimeHours = 10;
+//		double totalWages;
+//		regularWages = basePay * regularHours;
+//		overtimeWages = overtimePay * overtimeHours;
+//		totalWages = regularWages + overtimeHours;
+//		System.out.println("Wyp³ata za ten tydzieñ wynosi " + totalWages + " z³otyc.");
+// Program oblicza kwotê odk³adana wed³ug planu emerytalnego
+//		double wyplataMiesiac = 6000.0;
+//		double wklad;
+//// 5% pensji odk³adanej mieisêcznie.
+//		wklad = wyplataMiesiac * 0.05;
+//		System.out.println("Odk³adanie 5% daje " + wklad + "z³otych miesiêcznie.");
+//// 8% pensji odk³adanej mieisêcznie.
+//		wklad = wyplataMiesiac * 0.08;
+//		System.out.println("Odk³adanie 8% daje " + wklad + "z³otych miesiêcznie.");
+//// 10% pensji odk³adanej mieisêcznie.
+//		wklad = wyplataMiesiac * 0.10;
+//		System.out.println("Odk³adanie 10% daje " + wklad + "z³otych miesiêcznie.");
+// Rzutowanie typów danych
+//		int liczba = 1;
+//		char znak;
+//		znak = (char)liczba;
+//		System.out.println("Teraz to jest znak " + znak);
+// Zmienne
+//		final double PRICE = 19.99;
+//		final double INTEREST_RATE = 0.069;
+//		System.out.println("Cena wynosi " + PRICE + " .");
+//2.8 Klasa String
+		String pozdrowienie = "Dzieñ dobry! ";
+		String imie = "Henryku.";
+		int stringSize;
+		System.out.println(pozdrowienie + imie);
+		imie = "Henryk";
+		stringSize = imie.length();
+		System.out.println("Imie: " + imie + " ma " + stringSize + " znaków.");
+		System.out.println("Tylko ma³e znaki: " + imie.toLowerCase());
+		System.out.println("Tylko DU¯E znaki: " + imie.toUpperCase());		
+// Komentarze
+// I. Jednowierszowe
+// ***************************************************************************
+// PROGRAM: Comment1.java
+// Autor: Mateusz Bukowski
+// Ten program oblicza p³ace w firmie.
+		
+		double payRate;     // Przechowywanie stawki godzinowej
+		double hours;       // Przechwywanie liczby przepracowanych godzin
+		int employeeNumber; // Przechowywanie numeru pracownika
+		
+		// Resztê programu pominiêto.
+// II. Wielowierszowe	
+// ***************************************************************************
+/* PROGRAM: Comment1.java
+ * Autor: Mateusz Bukowski
+ * Ten program oblicza p³ace w firmie.
+ */
 				
-			
-	}
+				double payRate2;     // Przechowywanie stawki godzinowej
+				double hours2;       // Przechwywanie liczby przepracowanych godzin
+				int employeeNumber2; // Przechowywanie numeru pracownika
+				
+				// Resztê programu pominiêto.
+// ***************************************************************************
+/* 
+PROGRAM: Comment1.java
+Autor: Mateusz Bukowski
+Ten program oblicza p³ace w firmie.
+*/
+	double payRate3;     // Przechowywanie stawki godzinowej
+	double hours3;       // Przechwywanie liczby przepracowanych godzin
+	int employeeNumber3; // Przechowywanie numeru pracownika
+								
+// Resztê programu pominiêto.
+	
+	
+	
+/////////////////////////////////////////////
+// PROGRAM: Comment1.java
+// Autor: Mateusz Bukowski
+// Ten program oblicza p³ace w firmie.
+////////////////////////////////////////////
+	
+// III. Komentarze u¿ywane jako dokumentacja (komentarze JAVADOC)
+// Mog¹ byæ wczytywane i przetwa¿ane przez program javadoc
 
+/**
+ * Ta klasa tworzy program obliczaj¹cy pensje w firmie.
+ */
+
+/**
+ * Metoda main jest punktem wejœcia do programu
+ */
+	
+	
+	
+	
+	
+	
+		
+	}
+ 
 }
+
+
+
+
+
+
+
+
+
