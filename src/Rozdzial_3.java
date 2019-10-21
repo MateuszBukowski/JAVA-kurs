0d849884570b805c72f6095cfe0c2e040c826955
Mateusz Bukowski <mateuszbukowski13@gmail.com>
2019-10-21 12:42:11 +0200

    [Rozdział 3] - Ukończone 3.1

--- src/Rozdzial_3.java
+++ src/Rozdzial_3.java
@@ -1,10 +1,46 @@
+import java.util.Scanner;
 
 public class Rozdzial_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("test");
-
+/*
+ * Rozdział 3.
+ */
+		
+//// 3.1 IF
+//		double score1,  // Wynik nr 1
+//			   score2,  // Wynik nr 2
+//			   score3,  // Wynik nr 3
+//			   average; // Średni wynik
+//		
+//		// Tworzenie obiektu klasy Scanner do wczytywania danych wejściowych.
+//		Scanner keyboard = new Scanner(System.in);
+//		
+//		System.out.println("Ten program oblicza średnią wyników trzech testów.");
+//		
+//		//Pobranie pierwszego wyniku
+//		System.out.print("Wprowadź wynik nr 1: ");
+//		score1 = keyboard.nextDouble();
+//		
+//		//Pobranie pierwszego wyniku
+//		System.out.print("Wprowadź wynik nr 2: ");
+//		score2 = keyboard.nextDouble();
+//				
+//		//Pobranie pierwszego wyniku
+//		System.out.print("Wprowadź wynik nr 3: ");
+//		score3 = keyboard.nextDouble();
+//				
+//		// Obliczanie i wyświetlanie średniego wyniku.
+//		average = (score1 + score2 + score3) / 3.0;
+//		
+//		System.out.println("Średnia wynosi " + average + ".");
+//		
+//		// Jeśli średnia przekracza 95, należy pogratulować użytkownikowi.
+//		if (average > 95)
+//			System.out.println("To świetny wynik!");
+//		
+// 3.2 IF-ELSE
+		
 		
 	}
 
