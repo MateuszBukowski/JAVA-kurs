acd0273f02fd26aa4c28a583fb34184fa2ab1268
Mateusz Bukowski <mateuszbukowski13@gmail.com>
2019-10-29 14:28:16 +0100

    [Rozdział 4] - ukiończone 4.1 i 4.2

--- src/Rozdzial_4.java
+++ src/Rozdzial_4.java
@@ -2,8 +2,44 @@
 public class Rozdzial_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * Rozdział 4.
+		 */
+//// 4.1.	Operatory inkrementacji i dekrementacji
+		/**
+		  Ten program ilustruje działanie operatorów ++ i −−.
+	*/
+//		int number = 4; // Początkowa wartość zmiennej number to 4.
+//		// Wyświetlanie wartości zmiennej number.
+//		System.out.println("Wartość zmiennej number to " + number + ".");
+//		System.out.println("Inkrementacja zmiennej number.");
+//		// Inkrementacja zmiennej number.
+//		number++;
+//		// Ponowne wyświetlanie wartości zmiennej number.
+//		System.out.println("Wartość zmiennej number to " + number + ".");
+//		System.out.println("Dekrementacja zmiennej number.");
+//		// Dekrementacja zmiennej number.
+//		number--;
+//		// Ponowne wyświetlanie wartości zmiennej number.
+//		System.out.println("Wartość zmiennej number to " + number + ".");
+//// 4.2.	Pętla while
+//		int number = 1;
+//		while (number <= 5)
+//		{
+//			System.out.println("Witaj" + number);
+//			number++;
+//		}
+//		System.out.println("To już wszystko!");
+//// 4.3.	Stosowanie pętli while do sprawdzania poprawności danych wejściowych
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
