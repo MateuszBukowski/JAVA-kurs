4a41ad2323af03fccc14f77a2db03ba212f8cf4d
Mateusz Bukowski <mateuszbukowski13@gmail.com>
2019-11-04 14:47:48 +0100

    [Rozdział 5] - 5.1 ukończone.

--- src/Rozdzial_4.java
+++ src/Rozdzial_4.java
@@ -139,42 +139,44 @@ public class Rozdzial_4 {
 //// 4.8.	Instrukcje break i continue (opcjonalne)
 		
 //// 4.10.	Wprowadzenie do zapisu i odczytu plików
-		/**
-		 * Ten program zapisuje dane w pliku.
-		 */
-		String filename;    // Nazwa pliku.
-		String friendName;  // Imię znajomego.
-		int numFriends;     // Liczba znajomych.
+//		/**
+//		 * Ten program zapisuje dane w pliku.
+//		 */
+//		String filename;    // Nazwa pliku.
+//		String friendName;  // Imię znajomego.
+//		int numFriends;     // Liczba znajomych.
+//		
+//		// Tworzenie obiektu klasy Scanner do pobierania danych z klawiatury.
+//		Scanner keyboard = new Scanner(System.in);
+//		
+//		// Pobieranie liczby znajomych.
+//		System.out.print("Ilu masz znajomych? ");
+//		numFriends = keyboard.nextInt();
+//		
+//		// Pobieranie pozostawionego znaku nowego wiersza.
+//		keyboard.nextLine();
+//		
+//		// Pobieranie nazwy pliku.
+//		System.out.print("Podaj nazwę pliku: ");
+//		filename = keyboard.nextLine();
+//		
+//		// Otwieranie pliku.
+//		PrintWriter outputFile = new PrintWriter(filename);
+//		
+//		// Pobieranie danych i zapisywanie ich w pliku.
+//		for (int i = 1; i <= numFriends; i++)
+//		{
+//			// Pobieranie imienia znajomego.
+//			System.out.print("Podaj imię znajomego " + "numer " + i + ": ");
+//			friendName = keyboard.nextLine();
+//			// Zapisywanie imienia w pliku.
+//			outputFile.println(friendName);
+//		}
+//		// Zamykanie pliku.
+//		outputFile.close();
+//		System.out.println("Dane zostały zapisane w pliku.");
 		
-		// Tworzenie obiektu klasy Scanner do pobierania danych z klawiatury.
-		Scanner keyboard = new Scanner(System.in);
 		
-		// Pobieranie liczby znajomych.
-		System.out.print("Ilu masz znajomych? ");
-		numFriends = keyboard.nextInt();
-		
-		// Pobieranie pozostawionego znaku nowego wiersza.
-		keyboard.nextLine();
-		
-		// Pobieranie nazwy pliku.
-		System.out.print("Podaj nazwę pliku: ");
-		filename = keyboard.nextLine();
-		
-		// Otwieranie pliku.
-		PrintWriter outputFile = new PrintWriter(filename);
-		
-		// Pobieranie danych i zapisywanie ich w pliku.
-		for (int i = 1; i <= numFriends; i++)
-		{
-			// Pobieranie imienia znajomego.
-			System.out.print("Podaj imię znajomego " + "numer " + i + ": ");
-			friendName = keyboard.nextLine();
-			// Zapisywanie imienia w pliku.
-			outputFile.println(friendName);
-		}
-		// Zamykanie pliku.
-		outputFile.close();
-		System.out.println("Dane zostały zapisane w pliku.");
 	}
 
 }
---
+++ src/Rozdzial_5.java
@@ -0,0 +1,21 @@
+
+public class Rozdzial_5 {
+	//// 5.1
+	
+
+	public static void main(String[] args) {
+		/**
+		 * W tym programie definiowana i wywoływana jest prosta metoda.
+		 */
+		System.out.println("Powitanie przez metodę main.");
+		displayMessage();
+		System.out.println("Z powrotem w metodzie main.");
+		
+		/**
+		 * Wyświetlanie powitania przez metodę displayMessage.
+		 */		
+	}
+	public static void displayMessage() {
+		System.out.println("Powitanie przez metodę displayMessage.");
+	}
+}
