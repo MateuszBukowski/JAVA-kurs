fe44531f32b9c316af84599147451f3ab63d3c18
Mateusz Bukowski <mateuszbukowski13@gmail.com>
2019-11-07 21:02:17 +0100

    [Rozdziała 6] -  Ukończone 6.1 i 6.2. Stanołem na Linsting 6.3. Utworzyłem plik klasy JAVA-kurs/src/Rectangle.java

---
+++ src/Rectangle.java
@@ -0,0 +1,24 @@
+/*
+ * Klasa Rectangle w trakcie postawania
+*/
+
+public class Rectangle
+{
+	
+	private double length;
+	private double width;
+
+
+/**
+ Metoda setLength zapisuje wartoœæ w polu length.
+ @param len Wartoœæ zapisana w polu length.
+*/
+
+public void setLength(double len)
+{
+
+	length = len;
+
+}
+
+}
